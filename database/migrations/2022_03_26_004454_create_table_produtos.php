8d3177baa526c89f2686683ce10aeed0e1c013e8
sirjohnsx <95320687+sirjohnsx@users.noreply.github.com>
2022-03-25 21:49:58 -0300

    up correcoes

---
+++ database/migrations/2022_03_26_004454_create_table_produtos.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableProdutos extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_produtos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->float('valor',8,2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_produtos');
+    }
+}
