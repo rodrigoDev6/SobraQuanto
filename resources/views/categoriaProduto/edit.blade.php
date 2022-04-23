0531f4c11ec1f9f33dc70de95bc8029846ab8548
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2022-04-22 21:22:56 -0300

    updade

--- resources/views/categoriaProduto/edit.blade.php
+++ resources/views/categoriaProduto/edit.blade.php
@@ -2,12 +2,9 @@
 
 @section('title', 'Sobra Quanto')
 
-@section('content_header')
-    <h1 class="text-center">Categoria Selecionada:</h1>
-@stop
 
 @section('content')
-<div class="container">
+    <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <h1 class="mb-3">Categoria selecionada:</h1>
@@ -15,33 +12,42 @@
                 <div class="card card-primary ">
                     <div class="card-header">{{ __('Dashboard') }}</div>
 
-<div class="card-body col-12">
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                    @endforeach
-            </ul>
+                    {{-- procurando categoria de produto --}}
+                    {{ Form::model($CategoriaProduto, ['route' => ['categoriaProduto.update', $CategoriaProduto->id],'method' => 'PUT']) }}
+                    <div class="card-body col-12">
+                        @if ($errors->any())
+                            <div class="alert alert-danger">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
+
+
+                        <div class="row col-12">
+                            <div class="form-group col">
+                                {{ Form::label('nome', 'Nome:') }}
+                                {{ Form::text('nome', $CategoriaProduto->nome, ['class' => 'form-control']) }}
+
+                                <div class="row col-12">
+                                    <div class="form-group col">
+                                        {{ Form::submit('Enviar', ['class' => 'btn btn-primary']) }}
+                                        <a class="btn btn-default float-right"
+                                            href="{{ route('categoriaProduto') }}">Cancelar</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {{-- card card-primary --}}
+            </div>
         </div>
-    @endif
-    
-    {{-- procurando categoria de produto --}}
-    {{ Form::model($CategoriaProduto, ['route' => ['categoriaProduto.update', $CategoriaProduto->id], 'method' => 'PUT']) }}
+    </div>
 
-
-    <div class="row col-12">
-    <div class="form-group col">
-    {{ Form::label('nome', 'nome') }}
-    {{ Form::text('nome', $CategoriaProduto->nome) }}
-    </div>
-    {{ Form::submit('Enviar') }}
-    </div>
-    </div>
-    </div>
-    </div>
-@stop
-
+@endsection
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
     <link rel="icon" href="img/sobraquanto.png">
