9e93c0f602519151033b422ca513eaa4e48ca2c1
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2022-04-26 21:50:04 -0300

    update pdv

--- resources/views/pdv/index.blade.php
+++ resources/views/pdv/index.blade.php
@@ -3,13 +3,46 @@
 @section('title', 'Sobra Quanto')
 
 @section('content_header')
-    <h1>Painel de venda</h1>
+    <h1>Painel de Vendas</h1>
 @stop
 
 @section('content')
     <p>PDV onde as vendas do caixa serão realizadas. Tela 10/43</p>
+
+    {{-- teste --}}
+    <div class="container-fluid">
+
+
+
+        <div class="row">
+            <aside class="col-lg-9">
+                <div class="card-body">
+                    Aqui ficarão os produtos vendidos
+                </div>
+            </aside>
+            <aside class="col-lg-3">
+                <div class="card">
+                    <div class="card-body">
+                        <h4>Carrinho vazio</h4>
+                        <p>Total: R$ 0,00</p>
+                        <p>Desconto: R$ 0,00</p>
+                        <hr>
+                        <div class="btn-group-vertical center">
+                            <a href="#" class="btn btn-success" type="button">Finalizar Pedido</a>
+                            <br>
+                            <a href="#" class="btn btn-danger" type="button">Cancelar</a>
+                        </div>
+                    </div>
+                </div>
+            </aside>
+        </div>
+    </div>
+    {{-- fim teste --}}
 @stop
 
 @section('css')
     <link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" href="/resources/css/pdv.css">
+    <link rel="icon" href="img/sobraquanto.png">
+
 @stop
