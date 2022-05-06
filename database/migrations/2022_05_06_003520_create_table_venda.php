82b6f00f08f11c4c10c0a4d87a4ca9b68e3346a7
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2022-05-05 21:45:41 -0300

    inicio painel de vendas

---
+++ database/migrations/2022_05_06_003520_create_table_venda.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableVenda extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venda', function (Blueprint $table) {
+            $table->id();
+            $table->integer('cliente_id');
+            $table->date('data');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venda');
+    }
+}
---
+++ database/migrations/2022_05_06_003545_create_table_venda_produto.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableVendaProduto extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('venda_produto', function (Blueprint $table) {
+            $table->id();
+            $table->integer('venda_id');
+            $table->integer('produto_id');
+            $table->integer('quantidade');
+            $table->float('valor', 8, 2);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('venda_produto');
+    }
+}
