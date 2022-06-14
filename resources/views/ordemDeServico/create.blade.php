dab1babe81c5ea0e4f9598c777cf7093c22e8b33
rodrigodev6 <rodrigolima.alves21@gmail.com>
2022-06-14 15:13:21 -0300

    up ordem

--- resources/views/ordemDeServico/create.blade.php
+++ resources/views/ordemDeServico/create.blade.php
@@ -46,50 +46,126 @@
                                 {{ Form::select('status', $status, '', ['class' => 'form-control', 'placeholder' => '-Escolha um status-']) }}
                             </div>
                             <div class="form-group col">
-                                {{ Form::label('Data Incial:', 'Data Incial:', ['class' => 'h5']) }}
-                                {{ Form::date('dataInicial', new \DateTime(), ['class' => 'form-control']) }}
+                                {{ Form::label('data_abertura', 'Abertura:', ['class' => 'h5']) }}
+                                {{ Form::date('data_abertura', new \DateTime(), ['class' => 'form-control']) }}
                             </div>
 
                             <div class="form-group col">
-                                {{ Form::label('Data Final:', 'Data Final:', ['class' => 'h5']) }}
-                                {{ Form::date('dataFinal', null, ['class' => 'form-control']) }}
+                                {{ Form::label('data_fechamento', 'Vencimento:', ['class' => 'h5']) }}
+                                {{ Form::date('data_fechamento', null, ['class' => 'form-control']) }}
                             </div>
 
 
                             <div class="form-group col">
                                 {{ Form::label('formaPagamento', 'Pagamento:', ['class' => 'h5']) }}
-                                {{ Form::select('formaPagamento', ['PIX' => 'Pix'], 1, ['class' => 'form-control', 'placeholder' => '-todos-']) }}
+                                {{ Form::select('formaPagamento', ['pix' => 'PIX', 'credito' => 'CARTÃO DE CRÉDITO', 'dinheiro' => 'DINHEIRO'], '', ['class' => 'form-control']) }}
                             </div>
                         </div>
 
-                        <div class="form-group p-4">
-                            {{ Form::label('descricao', 'Descrição:', ['class' => 'h5']) }}
-                            {{ Form::textarea('descricao', null, ['class' => 'form-control', 'rows' => '3']) }}
-                        </div>
-
-
-
-
-
-                        <div class="row col-12">
+                        <div class="row row-cols-4 p-4">
                             <div class="form-group col">
-                                {{ Form::submit('Enviar', ['class' => 'btn btn-primary']) }}
+                                {{ Form::label('servico', 'Serviço:', ['class' => 'h5']) }}
+                                {{ Form::select('servico', $servico, '', ['class' => 'form-control', 'placeholder' => '-Escolha um serviço-']) }}
+                            </div>
 
-                                <a class="btn btn-default float-right" href="{{ route('usuario') }}">Cancelar</a>
+                            <div class="form-group col">
+                                {{ Form::label('valor', 'Valor unitário:', ['class' => 'h5']) }}
+                                {{ Form::text('valor', '', ['class' => 'form-control']) }}
+                            </div>
+
+                            <div class="form-group col">
+                                {{ Form::label('quantidade', 'Quantidade:', ['class' => 'h5']) }}
+                                {{ Form::number('quantidade', '', ['class' => 'form-control']) }}
+                            </div>
+
+                            <div class="form-group"
+                                style="text-align: center;display: flex;flex-direction: column;justify-content: flex-end;">
+                                {{-- add plus --}}
+                                <button type="button" class="btn btn-primary" style="width: 20%;">
+                                    <i class="fas fa-plus"></i>
+                                </button>
                             </div>
                         </div>
 
-                        {{ Form::close() }}
+                        <div class="form-group">
+                            <div class="card shadow-none">
+                                <div class="card-body">
+                                    <table class="table">
+                                        <thead>
+                                            <tr>
+                                                <th scope="col">Serviço</th>
+                                                <th scope="col">Valor</th>
+                                                <th scope="col">Quantidade</th>
+                                                <th scope="col">Total</th>
+                                                <th scope="col"></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Formatação</td>
+                                                <td>R$80</td>
+                                                <td>1</td>
+                                                <td>R$ 80</td>
+                                                <td>
+                                                    <button type="button" class="btn btn-danger">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Formatação</td>
+                                                <td>R$80</td>
+                                                <td>1</td>
+                                                <td>R$ 80</td>
+                                                <td>
+                                                    <button type="button" class="btn btn-danger">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>Formatação</td>
+                                                <td>R$80</td>
+                                                <td>1</td>
+                                                <td>R$ 80</td>
+                                                <td>
+                                                    <button type="button" class="btn btn-danger">
+                                                        <i class="fas fa-trash"></i>
+                                                    </button>
+                                                </td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+
+                                </div>
+                            </div>
+
+
+                            <div class="form-group p-4">
+                                {{ Form::label('observacao', 'Observação:', ['class' => 'h5']) }}
+                                {{ Form::textarea('observacao', null, ['class' => 'form-control', 'rows' => '3']) }}
+                            </div>
+
+                            <div class="form-group p-4">
+                                {{ Form::submit('Salvar', ['class' => 'btn btn-primary']) }}
+                                {{ Form::reset('Limpar', ['class' => 'btn btn-danger']) }}
+                                {{ Form::button('Cancelar', ['class' => 'btn btn-secondary', 'onclick' => 'history.go(-1)']) }}
+                            </div>
+
+                            {{ Form::close() }}
+
+
+                        </div>
+                        {{-- card-primary-end --}}
+
+
+
                     </div>
-
-
                 </div>
             </div>
-        </div>
-    </div>
-@endsection
+        @endsection
 
-@section('css')
-    <link rel="stylesheet" href="/css/admin_custom.css">
-    <link rel="icon" href="img/sobraquanto.png">
-@stop
+        @section('css')
+            <link rel="stylesheet" href="/css/admin_custom.css">
+            <link rel="icon" href="img/sobraquanto.png">
+        @stop
